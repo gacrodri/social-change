47f226aa349af0de4b15cd6ab3bc17d0674a20d2
Gabriela Rodriguez <grodrigue084@gmail.com>
2019-07-24 17:51:18 -0700

    login/sign out link

--- main.py
+++ main.py
@@ -18,12 +18,14 @@ import webapp2
 import os
 import jinja2
 from models import Food
+from google.appengine.api import users
 
 #remember, you can get this by searching for jinja2 google app engine
 jinja_current_dir = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
 
 class FoodHandler(webapp2.RequestHandler):
     def get(self):
@@ -32,11 +34,11 @@ class FoodHandler(webapp2.RequestHandler):
 
     def post(self):
         the_fav_food = self.request.get('user-fav-food')
-        
+
         #put into database (optional)
         food_record = Food(food_name = the_fav_food)
         food_record.put()
-        
+
         #pass to the template via a dictionary
         variable_dict = {'fav_food_for_view': the_fav_food}
         end_template = jinja_current_dir.get_template("templates/results.html")
@@ -49,7 +51,27 @@ class ShowFoodHandler(webapp2.RequestHandler):
         dict_for_template = {'top_fav_foods': fav_foods}
         self.response.write(food_list_template.render(dict_for_template))
 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        # [START user_details]
+        main_temp = jinja_current_dir.get_template("templates/welcome.html")
+        my_user = users.get_current_user()
+
+        if my_user:
+            auth_url = users.create_logout_url('/') #goes back to main page when logged out
+            greeting = 'Sign Out'
+        else:
+            auth_url = users.create_login_url('/')
+            greeting = 'Sign In'
+        # [END user_details]
+        my_dict = {
+            'auth_url': auth_url,
+            'greeting': greeting
+        }
+        self.response.write(main_temp.render(my_dict))
+
 app = webapp2.WSGIApplication([
-    ('/', FoodHandler),
+    ('/', MainPage),
+    ('/food', FoodHandler),
     ('/showfavs', ShowFoodHandler)
 ], debug=True)
